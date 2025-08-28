5095dab29e90c4df0bafd773f4a67f891c8f7d31
Julian Suk <sukju@selene.aim.cit.tum.de>
2025-08-28 14:57:52 +0200

    enable GPU-parallel training

--- lab_gatr/nn/attn_mask.py
+++ lab_gatr/nn/attn_mask.py
@@ -11,6 +11,6 @@ def get_attn_mask(target_batch: torch.Tensor, source_batch=None):
         attn_mask = BlockDiagonalMask.from_seqlens(
             q_seqlen=torch.bincount(target_batch).tolist(),
             kv_seqlen=torch.bincount(source_batch).tolist() if source_batch is not None else None
-        )
+        ).to(target_batch.device)
 
     return attn_mask
